4fdc0d696f0d124d9dd5e619b7f65f85ac61f590
Gordon Seto <gordon.seto@gmail.com>
2016-08-15 14:54:49 -0600

    added note saving and deleting

--- app/src/main/java/com/gordonseto/noteskeeper/EditActivity.java
+++ app/src/main/java/com/gordonseto/noteskeeper/EditActivity.java
@@ -1,12 +1,20 @@
 package com.gordonseto.noteskeeper;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
-public class EditActivity extends AppCompatActivity {
+import java.util.HashSet;
+
+public class EditActivity extends AppCompatActivity implements TextWatcher {
+
+    int noteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +24,37 @@ public class EditActivity extends AppCompatActivity {
         EditText editText = (EditText)findViewById(R.id.editText);
 
         Intent intent = getIntent();
-        int noteId = intent.getIntExtra("noteId", -1);
+        noteId = intent.getIntExtra("noteId", -1);
 
         if (noteId != -1){
             editText.setText(MainActivity.notes.get(noteId));
         }
+
+        editText.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        MainActivity.notes.set(noteId, charSequence.toString());
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        if (MainActivity.set == null){
+            MainActivity.set = new HashSet<String>();
+        }
+        MainActivity.set.clear();
+        MainActivity.set.addAll(MainActivity.notes);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.gordonseto.noteskeeper", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putStringSet("notes", MainActivity.set).apply();
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 }
--- app/src/main/java/com/gordonseto/noteskeeper/MainActivity.java
+++ app/src/main/java/com/gordonseto/noteskeeper/MainActivity.java
@@ -1,11 +1,16 @@
 package com.gordonseto.noteskeeper;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,10 +19,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+    static Set<String> set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +37,94 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView)findViewById(R.id.listView);
 
-        notes.add("Example note");
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.gordonseto.noteskeeper", Context.MODE_PRIVATE);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        set = sharedPreferences.getStringSet("notes", null);
+
+        notes.clear();
+        if (set != null){
+            notes.addAll(set);
+        } else {
+            notes.add("Example note");
+            set = new HashSet<String>();
+            set.addAll(notes);
+            sharedPreferences.edit().remove("notes").apply();
+            sharedPreferences.edit().putStringSet("notes", set).apply();
+        }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(), EditActivity.class);
-                intent.putExtra("noteId", i);
-                startActivity(intent);
+                goToNote(i);
             }
         });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long l) {
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(position);
+
+                                SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("com.gordonseto.noteskeeper", Context.MODE_PRIVATE);
+
+                                if (set == null) {
+                                    set = new HashSet<String>();
+                                } else {
+                                    set.clear();
+                                }
+
+                                set.addAll(notes);
+                                sharedPreferences.edit().remove("notes").apply();
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
+            }
+        });
+    }
+
+    public void saveSet(){
+
+    }
+
+    public void goToNote(int position) {
+        Intent intent = new Intent(getApplicationContext(), EditActivity.class);
+        intent.putExtra("noteId", position);
+        startActivity(intent);
+    }
+
+    public void createNewNote(){
+        notes.add("");
+        Log.i("MYAPP", notes.toString());
+        Log.i("MYAPP", String.valueOf(notes.size()));
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.gordonseto.noteskeeper", Context.MODE_PRIVATE);
+
+        if (set == null) {
+            set = new HashSet<String>();
+        } else {
+            set.clear();
+        }
+
+        set.addAll(notes);
+        sharedPreferences.edit().remove("notes").apply();
+        sharedPreferences.edit().putStringSet("notes", set).apply();
+        arrayAdapter.notifyDataSetChanged();
+
+        goToNote(notes.size() -1);
+
     }
 
     @Override
@@ -57,7 +142,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.add) {
+            createNewNote();
             return true;
         }
 
